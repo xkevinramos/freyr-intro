07615cad18bae55f40ab7849cabe8fef9d229df0
Kevin Ramos <kevin@Kevins-MacBook-Pro.local>
2023-08-01 16:31:54 -0700

    Used JS DOM to add the skills and footer sections

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const today = new Date();
+let thisYear = today.getFullYear(); // get current year
+
+// create paragraph element within the footer 
+const footer = document.querySelector('footer');
+const copyright = document.createElement('p');  
+copyright.innerText = `Kevin A. Ramos ${thisYear}`;
+footer.appendChild(copyright);
+
+const skills = ['JavaScript', 'HTML', 'CSS', 'Python', 'Git'];
+
+// select the skills id
+skillsSection = document.querySelector("#skills");
+ 
+// select the unordered list element
+skillsList = skillsSection.querySelector('ul');
+
+// iterate through skills array and append as a new list item
+for(let i = 0; i < skills.length; i++) {
+    let skill = document.createElement("li");
+    skill.innerText = skills[i]
+    skillsList.appendChild(skill);
+}
+
