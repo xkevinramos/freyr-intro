1991bb33f11f20e933542ab09495334f21efdd1f
Kevin Ramos <kevin@Kevins-MacBook-Pro.local>
2023-08-08 19:17:35 -0700

    Lesson 4.3

--- js/index.js
+++ js/index.js
@@ -18,7 +18,40 @@ skillsList = skillsSection.querySelector('ul');
 // iterate through skills array and append as a new list item
 for(let i = 0; i < skills.length; i++) {
     let skill = document.createElement("li");
-    skill.innerText = skills[i]
+    skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
 
+let messageForm = document.getElementById("messageForm");
+messageForm.addEventListener("submit", function(e) {
+    e.preventDefault(); // prevent refreshing after submitting
+    let nameField = e.target.usersName.value;
+    let emailField = e.target.usersEmail.value;
+    let messageField = e.target.usersMessage.value;
+    console.log(nameField);
+    console.log(emailField);
+    console.log(messageField);
+
+    let messageSection = document.getElementById("messages");
+    let messageList = messageSection.querySelector("ul");
+
+    // create new list element for new entries
+    let newMessage = document.createElement("li");
+    newMessage.innerHTML = `<a href="mailto:${emailField}">${nameField}</a><span> wrote: ${messageField} </span>`;
+    
+    // create button to remove entries
+    let removeButton = document.createElement("button");
+    removeButton.innerText = "remove";
+    
+    // click functionality for remove button
+    removeButton.addEventListener("click", function(e) {
+        let entry = removeButton.parentNode;
+        entry.remove();
+    });
+    
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    
+    messageForm.reset(); // reset form fields after submitting
+});
