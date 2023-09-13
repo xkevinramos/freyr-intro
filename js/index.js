792de5c507e2ad6de61df91410b01e0eb4aa4863
Kevin Ramos <kevin@Kevins-MacBook-Pro.local>
2023-09-12 20:35:29 -0700

    Updated to fetch method

--- js/index.js
+++ js/index.js
@@ -56,21 +56,16 @@ messageForm.addEventListener("submit", function(e) {
     messageForm.reset(); // reset form fields after submitting
 });
 
-let githubRequest = new XMLHttpRequest();
-githubRequest.open('GET', 'https://api.github.com/users/xkevinramos/repos');
-githubRequest.send();
+fetch("https://api.github.com/users/xkevinramos/repos").then(res => {
+    return res.json()}).then(repositories => {
 
-githubRequest.onload = function () {
-    let repositories = JSON.parse(githubRequest.responseText);
+        let projectSection = document.getElementById("projects");
+        let projectList = projectSection.querySelector("ul");
 
-    let projectSection = document.getElementById("projects");
-    let projectList = projectSection.querySelector("ul");
-
-    // populate HTML project section
-    for(let i = 0; i < repositories.length; i++) {
-        let project = document.createElement("li");
-        project.innerText = repositories[i].name;
-        projectList.appendChild(project);
-    }
-
-};
+        // populate HTML project section
+        for(let i = 0; i < repositories.length; i++) {
+            let project = document.createElement("li");
+            project.innerText = repositories[i].name;
+            projectList.appendChild(project);
+        }
+    });
